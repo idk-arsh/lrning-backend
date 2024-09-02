5e84d320dd1c492c041ccfc753d67e9a71d8c336
idk-arsh <arsh9745774@gmail.com>
2024-09-02 12:01:10 -0400

    now

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'hello',
-  password: '8666',
-  port: 5432, 
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
 })
 export default pool;

a03339c75db0f72705dc7f0a7b31dfefb4332fa2
idk-arsh <arsh9745774@gmail.com>
2024-09-02 12:22:47 -0400

    ok

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const { Pool } = pg;
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  host: process.env.DB_HOST,
+  host: process.env.DB_URL,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT

8f5a320ac0ab14820ebe3b9f847e341f97f8d7ff
idk-arsh <arsh9745774@gmail.com>
2024-09-02 22:57:47 -0400

    all

--- db.js
+++ db.js
@@ -1,14 +1,18 @@
 import pg from 'pg';
 const { Pool } = pg;
 
+
+
 const pool = new Pool({
   user: 'hello_2bns_user',
-  host: 'dpg-cratsdjtq21c73cda4g0-a.oregon-postgres.render.com',
+  host: 'dpg-cratsdjtq21c73cda4g0-a',
   database: 'hello_2bns',
   password: 'TV15d9t9o1NXSVygIyZ23QX3NbfJVX0F',
   port: 5432,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
-
 
 async function testConnection() {
   try {

e045f845bf79de37365d8c7fa31b2613402276e9
idk-arsh <arsh9745774@gmail.com>
2024-09-02 21:09:04 -0400

    all done

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ app.get('/api/coursera', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
